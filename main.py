9cd9c726a7d7b7946f85b361fd2cbf707bdc9b79
MiyukiKun <81800569+MiyukiKun@users.noreply.github.com>
2021-06-27 15:10:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import time
 
 is_busy = False
 
+
 class Timer:
     def __init__(self, time_between=2):
         self.start_time = time.time()
@@ -18,11 +19,13 @@ class Timer:
             return True
         return False
 
+
 @bot.on(events.NewMessage(pattern=("/start")))
 async def start(event):
     await bot.send_message(event.chat_id, "Im Running")
 
-@bot.on(events.NewMessage(pattern=("/kang")))
+
+@bot.on(events.NewMessage(pattern=("/upload")))
 async def download_function(event):
     global is_busy
     if is_busy == True:
@@ -30,44 +33,54 @@ async def download_function(event):
         return
     is_busy = True
     try:
-        x = await event.get_reply_message()
-        thumb = await bot.download_media(x.photo)
-    except:
-        thumb = "thumb.png"
+        try:
+            x = await event.get_reply_message()
+            thumb = await bot.download_media(x.photo)
+            print(thumb)
+        except:
+            thumb = "thumb.png"
 
-    split = event.raw_text[6:]
-    reply = await event.reply("Downloading")
-    for_name = split.split("|")
-    url = for_name[0]
-    url = url.replace(' ','')
-    try:
-        name = for_name[1]
-    except:
-        name = url.split("/")[-1]
-    await downloader.DownLoadFile(url, 1024*10, reply, file_name=name)
-    await Upload(event,reply, name, thumb)
-    await reply.delete()
-    os.remove(name)
-    is_busy = False
+        split = event.raw_text[8:]
+        reply = await event.reply("Downloading")
+        for_name = split.split("|")
+        url = for_name[0]
+        url = url.replace(' ', '')
+        try:
+            name = for_name[1]
+        except:
+            name = url.split("/")[-1]
+        await downloader.DownLoadFile(url, 1024*10, reply, file_name=name)
+        await Upload(event, reply, name, thumb)
+        await reply.delete()
+        os.remove(name)
 
-async def Upload(event,reply, out, thumbnail):
+    except Exception as e:
+        await reply.edit(str(e))
+
+    finally:
+        is_busy = False
+
+
+async def Upload(event, reply, out, thumbnail):
     timer = Timer()
+
     async def progress_bar(downloaded_bytes, total_bytes):
         if timer.can_send():
             await reply.edit(f"Uploading... {human_readable_size(downloaded_bytes)}/{human_readable_size(total_bytes)}")
 
     with open(out, "rb") as f:
         ok = await upload_file(
-                client=bot,
-                file=f,
-                name=out,
-                progress_callback= progress_bar
-            )
+            client=bot,
+            file=f,
+            name=out,
+            progress_callback=progress_bar
+        )
     await bot.send_message(
-        event.chat_id, file=ok, 
-        force_document=True, 
+        event.chat_id, file=ok,
+        force_document=True,
         thumb=thumbnail
-    )   
+    )
+
 
 def human_readable_size(size, decimal_places=2):
     for unit in ['B', 'KB', 'MB', 'GB', 'TB', 'PB']:
@@ -76,6 +89,7 @@ def human_readable_size(size, decimal_places=2):
         size /= 1024.0
     return f"{size:.{decimal_places}f} {unit}"
 
+
 @bot.on(events.NewMessage(pattern=("/batch")))
 async def batch(event):
     global is_busy
@@ -83,27 +97,34 @@ async def batch(event):
         await event.reply("Im busy with a diffrent task right now, try again later")
         return
     is_busy = True
-    x = await event.get_reply_message()
-    msg = x.raw_text.split("\n\n")
-    try:    
-        thumb = await bot.download_media(x.photo)
-    except:
-        thumb = "thumb.png"
 
-    for i in msg:
-        reply = await event.reply(f"Downloading")
-        for_name = i.split("|")
-        url = for_name[0]
-        url = url.replace(' ','')
-        try:
-            name = for_name[1]
-        except:
-            name = url.split("/")[-1]
-        await downloader.DownLoadFile(url, 1024*10, reply, file_name=name)
-        await Upload(event,reply, name, thumb)
-        os.remove(name)
-        await reply.delete()
-    is_busy = False
+    try:
+        x = await event.get_reply_message()
+        msg = x.raw_text.split("\n\n")
+        thumb = await bot.download_media(x.photo)
+        if thumb == None:
+            thumb = "thumb.png"
+
+        for i in msg:
+            reply = await event.reply(f"Downloading")
+            for_name = i.split("|")
+            url = for_name[0]
+            url = url.replace(' ', '')
+            try:
+                name = for_name[1]
+            except:
+                name = url.split("/")[-1]
+            await downloader.DownLoadFile(url, 1024*10, reply, file_name=name)
+            await Upload(event, reply, name, thumb)
+            os.remove(name)
+            await reply.delete()
+
+    except Exception as e:
+        await event.reply(e)
+
+    finally:
+        is_busy = False
+
 
 @bot.on(events.NewMessage(pattern=("/setthumb")))
 async def thumb(event):
@@ -115,11 +136,26 @@ async def thumb(event):
         f.write(pic)
     await event.reply("Set as default thumbnail")
 
+
 @bot.on(events.NewMessage(pattern=("/clearthumb")))
 async def clear_thumb(event):
     with open("thumb.png", "w") as f:
         f.write("")
     await event.reply("cleared thumbnail")
+
+
+@bot.on(events.NewMessage(pattern=("/status")))
+async def status(event):
+    await event.reply(f"{is_busy = }")
+
+
+@bot.on(events.NewMessage(pattern=("/viewthumb")))
+async def view(event):
+    try:
+        await event.reply("current default thumbnail", file="thumb.png")
+    except:
+        await event.reply("No default thumbnail set")
+
 
 bot.start()
 
